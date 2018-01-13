044a0159b224ee7feae6ad6f1f066a7aff2183af
Bhashithe Abeysinghe <bhashithe@gmail.com>
2018-01-13 09:47:10 +0530

    Create downvote.php

---
+++ downvote.php
@@ -0,0 +1,23 @@
+<?php
+	session_start();
+	include 'lib/database.php';
+
+	$postid = $_GET['postid'];
+
+	$str = "SELECT votes FROM post WHERE post_id = $postid LIMIT 1";
+	$result = mysqli_query($conn, $str) or die("error");
+	$row = mysqli_fetch_assoc($result);
+	$votes = $row['votes'];
+
+	$votes--;
+
+	$str = "UPDATE post SET votes = $votes WHERE post_id=$postid";
+	$result = mysqli_query($conn, $str) or die("error");
+
+	$str = "SELECT votes FROM post WHERE post_id = $postid LIMIT 1";
+	$result = mysqli_query($conn, $str) or die("error");
+	$row = mysqli_fetch_assoc($result);
+	$votes = $row['votes'];
+
+	echo $votes;
+?>
